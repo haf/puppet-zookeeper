1067ef2c41c851f67dbbc4837e4c2b79a54621a8
Kris Buytaert <sdog@inuits.be>
2014-05-22 12:44:38 +0200

    Cleaning up

--- manifests/package.pp
+++ manifests/package.pp
@@ -20,31 +20,5 @@ class zookeeper::package inherits zookeeper::params {
   }
 
 
-  #  wget::fetch { 'download_zookeeper':
-  #  source      => $download_url,
-  #  destination => "/usr/local/src/zookeeper-$version.tar.bz",
-  #  before      => Exec['untar_zookeeper'],
-  #  require     => Class['wget'],
-  #}
-
-  #exec { 'untar_zookeeper':
-  #  command => "/bin/tar xzf /usr/local/src/zookeeper-$version.tar.bz",
-  #  cwd     => '/opt',
-  #  creates => "${zk_dir}-$version",
-  #}
-
-  #file { "${zk_dir}-$version":
-  #$  ensure  => directory,
-  # owner   => $user,
-  #  group   => $group,
-  #  recurse => true,
-  #  require => Exec['untar_zookeeper'],
-  #}
-
-  #file { $zk_dir:
-  #  ensure  => link,
-  #  target  => "${zk_dir}-$version",
-  #  require => File["${zk_dir}-$version"],
-  #}
 
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -11,21 +11,8 @@ class zookeeper::service(
   $bin_dir    = $zookeeper::bin_dir
   $log4j_prop = $zookeeper::log4j_prop
 
-  # https://github.com/globocom/zookeeper-centos-6/blob/master/redhat/zookeeper.init
-  # first time before untarring ZK, this isn't available:
-  # TODO: solve this ordering problem
   $classpath = globby_join("${bin_dir}/{../lib,..}/*.jar", ':')
   $log_conf = 'file:///etc/zookeeper/log4j.properties'
-
-  #  supervisor::service { 'zookeeper':
-  #  ensure      => 'present',
-  #  user        => $user,
-  #  group       => $group,
-  #  directory   => $data_dir,
-  #  command     => "/usr/bin/java -Dzookeeper.log.dir=${log_dir} -Dlog4j.configuration=$log_conf \
-  #-Dzookeeper.root.logger=${log4j_prop} -cp $classpath org.apache.zookeeper.server.quorum.QuorumPeerMain \
-  # ${etc_dir}/zoo.cfg",
-  #}
 
 
   service { 'zookeeper':
